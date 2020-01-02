25468ce5aa34d975f99b58561723f46d1dceb965
asgk912 <asgk912@gmail.com>
2020-01-02 13:50:28 -0800

    implement all three given actions

--- src/actions/search.js
+++ src/actions/search.js
@@ -5,7 +5,10 @@ import YOUTUBE_API_KEY from '../config/youtube.js';
 
 
 var handleVideoSearch = (q) => {
- 
+  return (dispatch) => searchYouTube({key: YOUTUBE_API_KEY, query: q}, (videos) =>{
+    dispatch(changeVideoList(videos));
+    dispatch(changeVideo(videos[0]));
+  });
   //TODO:  Write an asynchronous action to handle a video search!
 };
 
