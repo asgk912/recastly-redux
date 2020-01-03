525dbe73fab3d2c0acca90b82db57f6c98f2f118
asgk912 <asgk912@gmail.com>
2020-01-02 17:30:10 -0800

    import Provider of react-redux to App and minor error correction

--- src/reducers/currentVideo.js
+++ src/reducers/currentVideo.js
@@ -1,11 +1,11 @@
 import Redux from 'redux';
 
-var currentVideoReducer = (state = null, action) => {
+var currentVideoReducer = (state, action) => {
   switch (action.type) {
   case 'CHANGE_VIDEO':
     return action.video;
   default:
-    return state;
+    return null;
   }
 
 };
